493b1eed08e973eee81d1eec4509f330037139ab
Rinabella <github@rinabel.la>
2025-08-02 18:29:54 -0500

    First manual commit, adding ubuntu kernel args

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,13 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+# x13s arguments (adapted from ubuntu 25.10 daily as of aug 2nd 2025, may not be needed)
+rpm-ostree kargs --append=clk_ignore_unused
+rpm-ostree kargs --append=pd_ignore_unused
+rpm-ostree kargs --append=arm64.nopauth
+rpm-ostree kargs --append=console=tty0
+rpm-ostree kargs --append=vt.handoff=7
+rpm-ostree kargs --append=crashkernel=2G-4G:320M,4G-32G:512M,32G-64G:1024M,64G-128G:2048M,128G-:4096M
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
