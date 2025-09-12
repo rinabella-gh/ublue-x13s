533de2f8d17b69c392af641b4e14b0acb55c3b5b
Rinabella <github@rinabel.la>
2025-09-12 17:54:59 -0500

    exclude rootfiles

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,9 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 cp /ctx/jlinton-x13s.repo /etc/yum.repos.d/ # add communtiy repo (might be able to use copr but will need to retry)
-
+# KDE but ignore rootfiles
+dnf -y group install kde-desktop-environment --exclude=root-files
 # need a specific version of alsa-ucm not included in f42 yet, eventually should be able to update it but for now just including it
-dnf -y group install kde-desktop-environment
 dnf -y install /ctx/alsa-ucm-1.2.12-2.fc41.noarch.rpm
 dnf -y install x13s sway x13s firefox labwc sway-config-fedora wireplumber mesa-vulkan-drivers mesa-dri-drivers neovim NetworkManager-wifi pavucontrol pipewire-pulseaudio wireplumber flatpak git fuzzel tuigreet
 # firmware
