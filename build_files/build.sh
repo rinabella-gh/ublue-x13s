065a646cc9794e6d2b47ff468eecdd5c18677083
Rinabella <github@rinabel.la>
2025-09-12 18:21:07 -0500

    Proceed

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ cp /ctx/jlinton-x13s.repo /etc/yum.repos.d/ # add communtiy repo (might be able 
 mv /root /root.old
 dnf -y group install kde-desktop-environment
 # mv /root.old/.* /root - there are no files here
-mv /root.old/* /root
+#mv /root.old/* /root - also no files here
 # need a specific version of alsa-ucm not included in f42 yet, eventually should be able to update it but for now just including it
 dnf -y install /ctx/alsa-ucm-1.2.12-2.fc41.noarch.rpm
 dnf -y install x13s sway x13s firefox labwc sway-config-fedora wireplumber mesa-vulkan-drivers mesa-dri-drivers neovim NetworkManager-wifi pavucontrol pipewire-pulseaudio wireplumber flatpak git fuzzel tuigreet
